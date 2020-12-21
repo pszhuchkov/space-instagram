cf2ae6b4c4add10693f51a95caa93f5d50181fef
pzhuchkov <pzhuchkov@gmail.com>
2020-12-21 22:27:26 +0300

    Add uploading images to Instagram

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import requests
 import os
+import io
 from pathlib import Path
 from urllib.parse import urlparse
+from PIL import Image
+from dotenv import load_dotenv
+from instabot import Bot
 
 
 SPACEX_API_LATEST_LAUNCH = 'https://api.spacexdata.com/v4/launches/latest'
 HUBBLE_API_GET_IMAGE = 'http://hubblesite.org/api/v3/image/{}'
 HUBBLE_API_GET_IMAGES = 'http://hubblesite.org/api/v3/images'
-HUBBLE_COLLECTION = 'spacecraft'
+HUBBLE_COLLECTION = 'holiday_cards'
 IMAGES_FOLDER = 'images'
 
 
 def download_img(url, filename):
+    """Download an image and save a thumbnail"""
     response = requests.get(url, verify=False)
     response.raise_for_status()
-    with open(os.path.join(IMAGES_FOLDER, filename), 'wb') as file:
-        file.write(response.content)
+    save_thumbnail_jpg(response.content, filename)
 
 
 def fetch_spacex_last_launch():
@@ -23,20 +27,18 @@ def fetch_spacex_last_launch():
     response.raise_for_status()
     images_urls = response.json()['links']['flickr']['original']
     for number, url in enumerate(images_urls, 1):
-        extension = get_extension(url)
-        filename = f'space{number}.{extension}'
+        filename = f'space{number}'
         download_img(url, filename)
 
 
-def fetch_hubble_images(image_id):
+def fetch_hubble_image(image_id):
     response = requests.get(HUBBLE_API_GET_IMAGE.format(image_id))
     response.raise_for_status()
     image_files = response.json()['image_files']
     best_image = image_files[-1]
     scheme = urlparse(HUBBLE_API_GET_IMAGE).scheme
     best_image_url = f"{scheme}:{best_image['file_url']}"
-    extension = get_extension(best_image_url)
-    filename = f"{image_id}.{extension}"
+    filename = f"{image_id}"
     download_img(best_image_url, filename)
 
 
@@ -47,17 +49,43 @@ def fetch_hubble_collection(collection_name):
     collection_images = response.json()
     for image in collection_images:
         image_id = image['id']
-        fetch_hubble_images(image_id)
-        print(f"{image_id} обработано")
+        fetch_hubble_image(image_id)
 
 
-def get_extension(url):
-    extension = url.split('.')[-1]
-    return extension
+def save_thumbnail_jpg(image_bytes, filename):
+    extension = 'jpg'
+    size = 1080, 1080
+    mode = 'RGB'
+    try:
+        image = Image.open(io.BytesIO(image_bytes))
+        if image.mode != 'RGB':
+            image = image.convert(mode)
+        image.thumbnail(size)
+        image.save(
+            os.path.join(IMAGES_FOLDER, '.'.join((filename, extension)))
+        )
+    except Exception as e:
+        print(e)
+
+
+def upload_images(images_folder):
+    load_dotenv()
+    folder_files = os.listdir(images_folder)
+    jpg_images = filter(lambda x: x.endswith('.jpg'), folder_files)
+    bot = Bot()
+    bot.login(
+        username=os.getenv('IG_LOGIN'),
+        password=os.getenv('IG_PASS')
+    )
+    for jpg_image in jpg_images:
+        filepath = os.path.join(images_folder, jpg_image)
+        if os.path.isfile(filepath):
+            filename = os.path.splitext(jpg_image)[0]
+            bot.upload_photo(os.path.join(images_folder, jpg_image), filename)
 
 
 if __name__ == '__main__':
     Path(IMAGES_FOLDER).mkdir(exist_ok=True)
     fetch_spacex_last_launch()
     fetch_hubble_collection(HUBBLE_COLLECTION)
-
+    upload_images(IMAGES_FOLDER)

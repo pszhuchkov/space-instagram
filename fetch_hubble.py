b8b387e3d76df68b1bb9b046facc844fe7ccf6f3
pzhuchkov <pzhuchkov@gmail.com>
2020-12-22 00:48:04 +0300

    Divide the main script

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -4,11 +4,8 @@ import io
 from pathlib import Path
 from urllib.parse import urlparse
 from PIL import Image
-from dotenv import load_dotenv
-from instabot import Bot
 
 
-SPACEX_API_LATEST_LAUNCH = 'https://api.spacexdata.com/v4/launches/latest'
 HUBBLE_API_GET_IMAGE = 'http://hubblesite.org/api/v3/image/{}'
 HUBBLE_API_GET_IMAGES = 'http://hubblesite.org/api/v3/images'
 HUBBLE_COLLECTION = 'spacecraft'
@@ -20,15 +17,6 @@ def download_img(url, filename):
     response = requests.get(url, verify=False)
     response.raise_for_status()
     save_thumbnail_jpg(response.content, filename)
-
-
-def fetch_spacex_last_launch():
-    response = requests.get(SPACEX_API_LATEST_LAUNCH)
-    response.raise_for_status()
-    images_urls = response.json()['links']['flickr']['original']
-    for number, url in enumerate(images_urls, 1):
-        filename = f'space{number}'
-        download_img(url, filename)
 
 
 def fetch_hubble_image(image_id):
@@ -67,26 +55,6 @@ def save_thumbnail_jpg(image_bytes, filename):
         print(e)
 
 
-def upload_images(images_folder):
-    load_dotenv()
-    folder_files = os.listdir(images_folder)
-    jpg_images = filter(lambda x: x.endswith('.jpg'), folder_files)
-    bot = Bot()
-    bot.login(
-        username=os.getenv('IG_LOGIN'),
-        password=os.getenv('IG_PASS')
-    )
-    for jpg_image in jpg_images:
-        filepath = os.path.join(images_folder, jpg_image)
-        if os.path.isfile(filepath):
-            filename = os.path.splitext(jpg_image)[0]
-            bot.upload_photo(filepath, filename)
-            if bot.api.last_response.status_code != 200:
-                print(bot.api.last_response)
-
-
 if __name__ == '__main__':
     Path(IMAGES_FOLDER).mkdir(exist_ok=True)
-    fetch_spacex_last_launch()
     fetch_hubble_collection(HUBBLE_COLLECTION)
-    upload_images(IMAGES_FOLDER)
---
+++ upload_images.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from instabot import Bot
+
+
+IMAGES_FOLDER = 'images'
+
+
+def upload_images(images_folder):
+    load_dotenv()
+    folder_files = os.listdir(images_folder)
+    jpg_images = filter(lambda x: x.endswith('.jpg'), folder_files)
+    bot = Bot()
+    bot.login(
+        username=os.getenv('IG_LOGIN'),
+        password=os.getenv('IG_PASS')
+    )
+    for jpg_image in jpg_images:
+        filepath = os.path.join(images_folder, jpg_image)
+        if os.path.isfile(filepath):
+            filename = os.path.splitext(jpg_image)[0]
+            bot.upload_photo(filepath, filename)
+            if bot.api.last_response.status_code != 200:
+                print(bot.api.last_response)
+
+
+if __name__ == '__main__':
+    upload_images(IMAGES_FOLDER)

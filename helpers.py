53099d2d610927fa4668fb15b08919302f791442
pzhuchkov <pzhuchkov@gmail.com>
2020-12-23 00:38:59 +0300

    Rename arguments

--- helpers.py
+++ helpers.py
@@ -5,11 +5,11 @@ import os
 def get_parsed_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-if', '--images_folder', help='Каталог с изображениями',
+        '-i', '--images_folder', help='Каталог с изображениями',
         default=os.getenv('IMAGES_FOLDER', 'images')
     )
     parser.add_argument(
-        '-hc', '--hubble_collection', help='Коллекция Hubble',
+        '-c', '--hubble_collection', help='Коллекция Hubble',
         default=os.getenv('HUBBLE_COLLECTION', 'spacecraft')
     )
     return parser.parse_args()

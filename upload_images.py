1350ad77a36a75ce935fb9b2235eea6ab797b605
pzhuchkov <pzhuchkov@gmail.com>
2020-12-22 22:35:01 +0300

    Add removing temporary files

--- upload_images.py
+++ upload_images.py
@@ -5,19 +5,27 @@ from helpers import get_parsed_arguments
 
 
 def upload_images(images_folder):
-    load_dotenv()
     folder_files = os.listdir(images_folder)
-    jpg_images = filter(lambda x: x.endswith('.jpg'), folder_files)
-    bot = Bot()
-    bot.login(
-        username=os.getenv('INSTAGRAM_LOGIN'),
-        password=os.getenv('INSTAGRAM_PASSWORD')
-    )
-    for jpg_image in jpg_images:
-        filepath = os.path.join(images_folder, jpg_image)
-        if os.path.isfile(filepath):
-            filename = os.path.splitext(jpg_image)[0]
-            bot.upload_photo(filepath, filename)
+    jpg_images = list(filter(lambda x: x.endswith('.jpg'), folder_files))
+    try:
+        bot = Bot()
+        bot.login(
+            username=os.getenv('INSTAGRAM_LOGIN'),
+            password=os.getenv('INSTAGRAM_PASSWORD')
+        )
+        for jpg_image in jpg_images:
+            filepath = os.path.join(images_folder, jpg_image)
+            if os.path.isfile(filepath):
+                filename = os.path.splitext(jpg_image)[0]
+                bot.upload_photo(filepath, filename)
+    finally:
+        for jpg_image in jpg_images:
+            try:
+                os.remove(
+                    f'{os.path.join(images_folder, jpg_image)}.REMOVE_ME'
+                )
+            except FileNotFoundError:
+                os.remove(os.path.join(images_folder, jpg_image))
 
 
 if __name__ == '__main__':

d70ea4d2d52b91ad78a485aaf8d98010ccf8f718
pzhuchkov <pzhuchkov@gmail.com>
2020-12-24 09:58:53 +0300

    Refactor upload_images.py

--- upload_images.py
+++ upload_images.py
@@ -15,17 +15,19 @@ def upload_images(images_folder):
         jpg_images = filter(lambda x: x.endswith('.jpg'), folder_files)
         for image in jpg_images:
             filepath = os.path.join(images_folder, image)
-            if os.path.isfile(filepath):
-                filename = os.path.splitext(image)[0]
+            if not os.path.isfile(filepath):
+                continue
+
+            filename = os.path.splitext(image)[0]
+            try:
+                bot.upload_photo(filepath, filename)
+            finally:
                 try:
-                    bot.upload_photo(filepath, filename)
-                finally:
-                    try:
-                        os.remove(
-                            f'{os.path.join(images_folder, image)}.REMOVE_ME'
-                        )
-                    except FileNotFoundError:
-                        os.remove(os.path.join(images_folder, image))
+                    os.remove(
+                        f'{os.path.join(images_folder, image)}.REMOVE_ME'
+                    )
+                except FileNotFoundError:
+                    os.remove(os.path.join(images_folder, image))
     else:
         print('The directory does not exist')
 
